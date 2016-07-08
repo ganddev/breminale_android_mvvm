28769b047871dbfd035347dd06cc5cf2ae69fa5f
Bjoern Ahlfeld <b.ahlfeld@gmail.com>
2016-07-08 14:58:41 +0200

    Added styles for wifi only switch

--- app/src/main/java/de/ahlfeld/breminale/app/viewmodel/MoreViewModel.java
+++ app/src/main/java/de/ahlfeld/breminale/app/viewmodel/MoreViewModel.java
@@ -2,10 +2,14 @@ package de.ahlfeld.breminale.app.viewmodel;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.SwitchCompat;
 import android.view.View;
 
+import de.ahlfeld.breminale.app.utils.BreminaleConsts;
 import de.ahlfeld.breminale.app.view.ImprintActivity;
 import de.ahlfeld.breminale.app.view.LicenseActivity;
 import de.ahlfeld.breminale.app.view.PrivacyActivity;
@@ -16,12 +20,21 @@ import de.ahlfeld.breminale.app.view.PrivacyActivity;
 public class MoreViewModel implements ViewModel {
 
 
+    private static final String TAG = MoreViewModel.class.getSimpleName();
     private Context context;
 
     public MoreViewModel(@NonNull Context context) {
         this.context = context;
     }
 
+
+    public void onOnlyWifiClick(View view) {
+        SharedPreferences sharedPreferences= PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        SwitchCompat wifiSwitch = (SwitchCompat)view;
+        editor.putBoolean(BreminaleConsts.DOWNLOAD_IMAGES_ONLY_ON_WIFI, wifiSwitch.isChecked());
+        editor.commit();
+    }
 
     public void onLicensesClick(View view) {
         Intent intent = new Intent(context, LicenseActivity.class);
